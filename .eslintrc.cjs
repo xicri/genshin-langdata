e49435068f92df9dc66742cd708b8d5a2eb48098
Xicri <xicri@proton.me>
2022-06-25 16:17:23 +0900

    chore: setup ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,18 +3,12 @@
 module.exports = {
   root: true,
   extends: [
-    // "xicri/node", // TODO
-    "plugin:jsonc/recommended-with-json5",
+    "xicri/node",
   ],
 
   parserOptions: {
-    ecmaVersion: 2022,
     sourceType: "module",
   },
-
-  plugins: [
-    "jsonc",
-  ],
 
   // top-levet await is not supported yet in ESLint 7
   ignorePatterns: [ "scripts/build-json.mjs" ],
@@ -32,45 +26,7 @@ module.exports = {
     },
     {
       files: [ "*.test.js", "*.test.mjs" ],
-      // extends: "xicri/jest", // TODO
-      rules: {
-        // TODO
-        // "jest/expect-expect": [ "error", {
-        //   assertFunctionNames: [ "expect", "ok" ],
-        // }],
-        "jest/no-conditional-expect": "off",
-      },
-    },
-    {
-      files: [ "*.json", "*.json5", "*.jsonc" ],
-      parser: "jsonc-eslint-parser",
-      rules: {
-        "jsonc/array-bracket-spacing": [ "error", "always", {
-          objectsInArrays: false,
-          arraysInArrays: false,
-        }],
-        "jsonc/comma-style": [ "error", "last" ],
-        "jsonc/indent": [ "error", 2 ],
-        "jsonc/key-spacing": [ "error", {
-          beforeColon: false,
-          afterColon: true,
-          mode: "minimum",
-        }],
-        "jsonc/object-curly-spacing": [ "error", "always", {
-          arraysInObjects: false,
-          objectsInObjects: false,
-        }],
-      },
-    },
-    {
-      files: [ "*.json5" ],
-      parser: "jsonc-eslint-parser",
-      rules: {
-        "jsonc/comma-dangle": [ "error", {
-          arrays: "always-multiline",
-          objects: "always-multiline",
-        }],
-      },
+      extends: "xicri/jest",
     },
   ],
 };
