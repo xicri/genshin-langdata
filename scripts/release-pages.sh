96b2372541b83577b2ecc7b7c6dee116331b66a4
Xicri <xicri@proton.me>
2023-03-10 17:53:16 +0900

    ci: skip deployment to Cloudflare Pages on CI triggered by contributors

--- scripts/release-pages.sh
+++ scripts/release-pages.sh
@@ -8,6 +8,11 @@ if [[ -f "${DIRNAME}/../.env" ]]; then
   . "${DIRNAME}/../.env"
 fi
 
+if [[ "${CI}" == "true" && -z "${CLOUDFLARE_API_TOKEN-}" ]]; then
+  echo "Skipping deployment to Cloudflare Pages because credentials are not available for contributors"
+  exit 0
+fi
+
 if [[ "${NODE_ENV}" == "production" ]]; then
   npx wrangler pages publish ./dist --project-name=genshin-langdata
 else
