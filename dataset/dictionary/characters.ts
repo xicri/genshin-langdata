8d1bf61c34314c1b5703230c9cc3c5c0a34c5f85
Xicri <107226297+xicri@users.noreply.github.com>
2025-07-08 11:17:24 +0900

    doc: clarify Japanese note for Lumine

--- dataset/dictionary/characters.ts
+++ dataset/dictionary/characters.ts
@@ -21,7 +21,7 @@ export default [
     zhCN: "荧",
     zhTW: "熒",
     pronunciationJa: "ほたる",
-    notes: "女性主人公名。「Lumine」の発音は「ルミーン」と思われるが諸説あり。俗称として MC (Main Character の略) と呼ばれることもある。",
+    notes: "女性主人公名。英語版の \"Lumine\" の発音は「ルミーン」と思われるが諸説あり。俗称として MC (Main Character の略) と呼ばれることもある。",
     notesZh: "女主人公的名字。英文「Lumine」为「发光」的意思。",
     notesZhTW: "女主角的名字。英文「Lumine」為「發光」的意思。",
     tags: [ "character-main" ],
