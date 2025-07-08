cd1a0a617cce45b9e71910ebd3c9a356bc313558
Xicri <107226297+xicri@users.noreply.github.com>
2025-07-08 11:25:48 +0900

    doc: sync Japanese note with Chinese notes

--- dataset/dictionary/characters.ts
+++ dataset/dictionary/characters.ts
@@ -1945,7 +1945,7 @@ export default [
     zhCN: "百晓",
     zhTW: "百曉",
     tags: [ "liyue", "character-sub" ],
-    notes: "群玉閣の秘書の1人",
+    notes: "凝光の秘書の1人",
     notesZh: "凝光的秘书之一。",
     notesZhTW: "凝光的秘書之一。",
   },
