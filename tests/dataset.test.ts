5ca9e4b73bdda9ef63dc396911216d37e3284d90
Xicri <xicri@proton.me>
2025-07-27 08:18:36 +0900

    doc: remove comment that mismatches to the code
    This comment does not explain the code but the code modifications.
    The comment should be included as a commit message or Pull Request descriptions. not as a comment.

--- tests/dataset.test.ts
+++ tests/dataset.test.ts
@@ -645,11 +645,6 @@ test("if the each translations do not include characters from the other language
       expect(word.zhTW).not.toMatch(/[ぁ-んァ-ヴー]/);
     }
 
-    /*
-     * This is a fix for the following problem:
-     * The test takes too long to determine Japanese/Chinese unique characters.
-     */
-
     const wordsJA = langSpecificChars.map((char) => char.ja);
     const wordsCN = langSpecificChars.map((char) => char["zh-CN"]);
     const wordsJAwoTW = langSpecificChars
