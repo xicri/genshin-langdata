22925e8b39821ee52268aafe65fd00eb58adb8fe
Yasuhiro, Kaneko <yasuking0304@gmail.com>
2025-10-12 09:20:46 +0900

    Update dataset/dictionary/items.ts
    Co-authored-by: Xicri <107226297+xicri@users.noreply.github.com>

--- dataset/dictionary/items.ts
+++ dataset/dictionary/items.ts
@@ -2827,30 +2827,22 @@ export default [
     notes: "ナドクライの「伴月紀聞・３」クリアの報酬",
     tags: [ "nodkrai" ],
   },
-  /*
   {
-    en: "Wings of Companionship", // Note: Was it originally meant to be by advance reservation only?
+    en: "Wings of Companionship",
     ja: "見守りの翼",
   },
-  */
-  /*
   {
-    en: "Wings of Descension", // Note: Play Station Network only
+    en: "Wings of Descension",
     ja: "降臨の翼",
   },
-  */
-  /*
   {
-    en: "Wings of Delicacies", // Note: KFC Collaboration only
+    en: "Wings of Delicacies",
     ja: "盛宴の翼",
   },
-  */
-  /*
   {
-    en: "Wings of Fate's Course Intertwined", // Note: Was it originally meant to be by XBOX only?
+    en: "Wings of Fate's Course Intertwined",
     ja: "交錯する運命の翼",
   },
-  */
 
   //
   // Dresesings
